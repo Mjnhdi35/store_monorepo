451d8c86b016b5ddcdd1ffcb194d7eb77f906ff7
smoothiestudio <mjnhdi35@gmail.com>
2026-01-16 18:12:11 +0700

    initial project nodejs

---
+++ apps/web/app/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+// const a = 0;
+// a = '';
+
+console.log('web');
+</script>
+
+<style scoped></style>
---
+++ apps/web/nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/image', '@nuxt/eslint', '@nuxt/ui', '@nuxtjs/eslint-module'],
+  devtools: { enabled: true },
+  compatibilityDate: '2025-07-15',
+});
